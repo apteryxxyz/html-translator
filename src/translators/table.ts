bcbc7178adeef1fcbed576636dac856fe4088742
ApteryxXYZ <apteryx.xyz@gmail.com>
2022-06-04 17:17:13 +1200

    Clean table row content before use

--- src/translators/table.ts
+++ src/translators/table.ts
@@ -3,29 +3,26 @@
  */
 
 //Imports
-import {Translator} from '../types';
-import {table} from 'table';
-import {HTMLElement, NodeType} from "node-html-parser";
+import { Translator } from '../types';
+import { table } from 'table';
+import { HTMLElement, NodeType } from "node-html-parser";
 
 /**
  * Extract data from a row
  * @param row Row to extract from
  * @returns Row data
  */
-const extractRow = (row: HTMLElement) =>
-{
+const extractRow = (row: HTMLElement) => {
   const data: string[] = [];
 
   //Iterate over cells
-  for (const cell of row.childNodes)
-  {
+  for (const cell of row.childNodes) {
     //Only translate table cells 
     if (cell.nodeType == NodeType.ELEMENT_NODE &&
       (
         (cell as HTMLElement).tagName == 'TD' ||
         (cell as HTMLElement).tagName == 'TH')
-    )
-    {
+    ) {
       //Extract the data
       data.push(cell.text);
     }
@@ -34,55 +31,51 @@ const extractRow = (row: HTMLElement) =>
   return data;
 };
 
+function cleanString(text: string): string {
+  return text.replace(/\r\n/, '\n').replace('\t', '');
+}
+
 //Export
 export default {
   inline: false,
   tags: [
     'TABLE'
   ],
-  translate: element =>
-  {
+  translate: element => {
     let header: string | undefined;
     const data: string[][] = [];
 
     //Iterate over children
-    for (const child of element.childNodes)
-    {
+    for (const child of element.childNodes) {
       //Only translate element nodes
-      if (child.nodeType == NodeType.ELEMENT_NODE)
-      {
+      if (child.nodeType == NodeType.ELEMENT_NODE) {
         //Cast
         const tableChild = child as HTMLElement;
 
         //Get the header from the caption
-        if (tableChild.tagName == 'CAPTION')
-        {
+        if (tableChild.tagName == 'CAPTION') {
           //Extract the header
           header = child.text;
         }
         //Get the data from the head or body
-        else if (tableChild.tagName == 'THEAD' || tableChild.tagName == 'TBODY')
-        {
+        else if (tableChild.tagName == 'THEAD' || tableChild.tagName == 'TBODY') {
           //Iterate over rows
-          for (const row of tableChild.childNodes)
-          {
+          for (const row of tableChild.childNodes) {
             //Only translate table rows 
-            if (child.nodeType == NodeType.ELEMENT_NODE && (row as HTMLElement).tagName == 'TR')
-            {
+            if (child.nodeType == NodeType.ELEMENT_NODE && (row as HTMLElement).tagName == 'TR') {
               data.push(extractRow(row as HTMLElement));
             }
           }
         }
         //Get the data from rows
-        else if (tableChild.tagName == 'TR')
-        {
+        else if (tableChild.tagName == 'TR') {
           data.push(extractRow(tableChild));
         }
       }
     }
 
     //Generate the table
-    let markdown = table(data, {
+    let markdown = table(data.map(o => o.map(cleanString)), {
       header: header != null ? {
         alignment: 'center',
         content: header
